b1d14d0e435fdc5366df62e4dc2a609f0be99d45
Надежда Кузнецова <kuznecovanadezhd@gmail.com>
2022-01-12 20:00:29 +0300

    Добавьте файлы проекта.

---
+++ Number10/Angle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number10
+{
+    class Angle
+    {
+        int gradus;
+        int minute;
+        int second;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    gradus = value % 360;
+            }
+        }
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    Gradus += value / 60;
+                minute = value % 60;
+            }
+        }
+
+       // internal int ToRadians()
+        //{
+            //throw new NotImplementedException();
+        //}
+
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+        public Angle(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public  double ToRadians(/*double degAngle*/)
+        { return Math.PI * (Gradus + 60 * Minute + 3600 * Second) / 180; }
+    }
+}
+    
---
+++ Number10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("введите данные");
+                int gradus_ = Convert.ToInt32(Console.ReadLine());
+                int minute_ = Convert.ToInt32(Console.ReadLine());
+                int second_ = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(gradus_, minute_, second_);
+            }
+            while (angle.isCorrect == false);
+
+            double totalSecond = angle.ToRadians();
+            Console.WriteLine(totalSecond);
+            Console.ReadKey();
+        }
+    }
+}
+       
